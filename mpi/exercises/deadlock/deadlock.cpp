3bcd7b08cc9ad01be8c374af0e1292d21d0c08ec
Gaelle Usseglio <usseglio.gaelle@gmail.com>
2022-05-16 16:13:11 +0200

    Add solutions for deadlock exercice

--- mpi/exercises/deadlock/deadlock.cpp
+++ mpi/exercises/deadlock/deadlock.cpp
@@ -3,9 +3,11 @@
 
 int main(int argc, char *argv[])
 {
-    int numtasks, rank, dest, source, rc, count, tag=1;  
+    int numtasks, rank, dest, source, rc, count, tag=1;
     char inmsg, outmsg='x';
+    int flag;
     MPI_Status Stat;
+    MPI_Request request = MPI_REQUEST_NULL;
 
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
@@ -15,15 +17,33 @@ int main(int argc, char *argv[])
       {
         dest = 1;
         source = 1;
-        rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
-        rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+        rc = MPI_Irecv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &request);
+        MPI_Test(&request, &flag, &Stat);
+
+        rc = MPI_Isend(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD, &request);
+
+        // re try to recv from rank 1 if failed (standard recv)
+        if (! flag)
+          {
+            rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+          }
     }
     else if (rank == 1)
       {
         dest = 0;
         source = 0;
-        rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
-        rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+        MPI_Status Stat_retry;
+        rc = MPI_Irecv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &request);
+        MPI_Wait(&request, &Stat);
+
+        rc = MPI_Isend(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD, &request);
+        MPI_Test(&request, &flag, &Stat_retry);
+        // Send twice to make sure to receive information
+        if (flag)
+          {
+            rc = MPI_Isend(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD, &request);
+          }
+
       }
 
     rc = MPI_Get_count(&Stat, MPI_CHAR, &count);
