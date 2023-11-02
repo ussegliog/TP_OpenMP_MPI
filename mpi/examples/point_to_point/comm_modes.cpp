922ff5c6804d6abd3cfb8627232ca4e8c0a3c286
Gaelle Usseglio <usseglio.gaelle.com>
2023-11-02 08:52:46 +0100

    Fix MPI_BSEND_OVERHEAD

--- mpi/examples/point_to_point/comm_modes.cpp
+++ mpi/examples/point_to_point/comm_modes.cpp
@@ -8,17 +8,27 @@ void buffered_send_recv(int rank, char *buf, int bufsize, char *inmsg,
 {
   int count, rc;
   MPI_Status Stat;
-  
+
   if (rank == 0)
     {
       std::cout  << "Buffered ...." << std::endl;
-      MPI_Buffer_attach(buf, bufsize);
+
+      int buffer_size_overhead = (MPI_BSEND_OVERHEAD + sizeof(char)) + (MPI_BSEND_OVERHEAD + 2 * sizeof(char));
+
+      char *buf_overhead = new char[buffer_size_overhead];
+
+      std::cout  << "MPI_BSEND_OVERHEAD : " << MPI_BSEND_OVERHEAD << std::endl;
       
+      MPI_Buffer_attach(buf_overhead, buffer_size_overhead);
+
       MPI_Bsend(outmsg.c_str(), bufsize, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
-      
+
       std::cout << "Task " << rank << " buffered send buffered , process will block on detach " << std::endl;;
 
-      MPI_Buffer_detach(buf, &bufsize); 
+      MPI_Buffer_detach(buf_overhead, &buffer_size_overhead);
+      
+      delete buf_overhead;
+      buf_overhead = 0;
     }
   else
     {
@@ -26,7 +36,7 @@ void buffered_send_recv(int rank, char *buf, int bufsize, char *inmsg,
       rc = MPI_Get_count(&Stat, MPI_CHAR, &count);
       std::cout << "Task  "<< rank << " : Received " << count << " char(s) from task " <<
         Stat.MPI_SOURCE << " with tag " <<  Stat.MPI_TAG << " and message : " << std::string(inmsg) << std::endl;
-      
+
     }
 
 }
@@ -36,7 +46,7 @@ void standard_send_recv(int rank, char *buf, int bufsize, char *inmsg,
 {
   int count, rc;
   MPI_Status Stat;
-  
+
   if (rank == 0)
     {
       std::cout  << "Standard ...." << std::endl;
@@ -49,7 +59,7 @@ void standard_send_recv(int rank, char *buf, int bufsize, char *inmsg,
       rc = MPI_Get_count(&Stat, MPI_CHAR, &count);
       std::cout << "Task  "<< rank << " : Received " << count << " char(s) from task " <<
         Stat.MPI_SOURCE << " with tag " <<  Stat.MPI_TAG << " and message : " << std::string(inmsg) << std::endl;
-      
+
     }
 
 }
@@ -75,14 +85,14 @@ int main(int argc, char** argv) {
   char* inmsg;
   std::string outmsg = "Testing";
   int bufsize = outmsg.length();
-  char *buf = new char[bufsize]; 
+  char *buf = new char[bufsize];
   inmsg = new char[bufsize];
 
   // Send the same mesage with different modes
   buffered_send_recv(rank, buf, bufsize, inmsg, outmsg);
   standard_send_recv(rank, buf, bufsize, inmsg, outmsg);
 
-  
+
   delete buf;
   delete inmsg;
   buf = 0;
